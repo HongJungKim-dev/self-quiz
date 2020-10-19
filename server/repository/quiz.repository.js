d87d58d615a740c20733c93ceb246d707151a029
woomin <dal96k@gmail.com>
2020-10-19 14:49:42 +0900

    [Modify] createNewQuiz 함수가 quiz._id를 반환하도록 변경
    퀴즈의 수정이나 삭제를 위해서는 유니크한 퀴즈의 아이디를
    클라이언트가 들고있어야하므로 퀴즈 아이디를 반환하도록 변경하였습니다.

--- server/repository/quiz.repository.js
+++ server/repository/quiz.repository.js
@@ -2,8 +2,10 @@ const Quiz = require('../models/quiz.js');
 
 module.exports = {
   async createNewQuiz(userId, question, answer, lastSolved, layer, tags) {
-    await Quiz.create({
+    const quiz = await Quiz.create({
       userId, question, answer, lastSolved, layer, tags,
     });
+
+    return quiz._id;
   },
 };
