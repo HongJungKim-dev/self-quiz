fb161ec2f2037393e8482da7a124bc0ca81c142e
woomin <dal96k@gmail.com>
2020-10-19 14:02:27 +0900

    [Refactor] errorCatcher라는 미들웨어 생성
    라우터에서 try-catch를 사용하면 계속 모든 라우터에 try-catch를 걸어줘야
    할 것 같아서 중복을 제거하기 위해 errorCatcher라는 미들웨어를 만들고
    에러를 라우터를 감싸도록 하였습니다. 이를 통해 라우터에서 발생하는
    에러는 errorCatcher가 잡아서 에러핸들러로 넘겨줍니다.

---
+++ server/middlewares/errorCatcher.js
@@ -0,0 +1,7 @@
+const errorCatcher = (router) => {
+  return (req, res, next) => {
+    router(req, res, next).catch(error => next(error));
+  };
+};
+
+module.exports = errorCatcher;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,16 +6,15 @@ const jwt = require('jsonwebtoken');
 
 const userRepo = require('../repository/user.repository.js');
 
-router.post('/signup', async (req, res, next) => {
+const errorCatcher = require('../middlewares/errorCatcher.js');
+
+router.post('/signup', errorCatcher(async (req, res) => {
   const { userId, userPw, userName } = req.body;
 
-  try {
-    await userRepo.signUpNewUser(userId, userPw, userName);
-    res.status(200).json({ message: 'Complete to sign up' });
-  } catch (error) {
-    next(error);
-  }
-});
+  await userRepo.signUpNewUser(userId, userPw, userName);
+
+  res.status(200).json({ message: 'Complete to sign up' });
+}));
 
 router.post('/login', (req, res) => {
   const { userId, userPw } = req.body;
