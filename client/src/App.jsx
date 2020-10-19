087102233821b0d9dcdd39fb489684cb2e5916e7
woomin <dal96k@gmail.com>
2020-10-19 11:24:57 +0900

    [Init] React를 사용한 클라이언트 환경 설정
    React를 통해 클라이언트를 구현하기 이전에 필요한 환경들을
    설정하였습니다.

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const styles = {
+  emotion: {
+    fontSize: '50px',
+    fontWeight: 'bold',
+    color: 'blue',
+  },
+};
+
+export default function App() {
+  return (
+    <div css={styles.emotion}>Quick Start React</div>
+  );
+}
