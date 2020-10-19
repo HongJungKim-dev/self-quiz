13f052a8a90e293bb061aef3b706ac2dd3cabb8b
woomin <dal96k@gmail.com>
2020-10-19 14:28:31 +0900

    [Feat] 퀴즈에 관한 MongoDB 스키마 작성
    Quiz에 관한 데이터베이스 테이블 생성 및 연결을 위해 Quiz 스키마를
    생성하였습니다.

---
+++ server/models/quiz.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const quizSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  question: { type: String, required: true },
+  answer: { type: String, required: true },
+  lastSolved: { type: String, required: true },
+  layer: { type: Number, required: true },
+  tags: { type: Array, required: true },
+});
+
+module.exports = mongoose.model('User', quizSchema);
