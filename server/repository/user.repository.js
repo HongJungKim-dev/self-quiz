4fa4165c5cfdba12c95a3fa003a36455ea91039b
woomin <dal96k@gmail.com>
2020-10-19 13:56:37 +0900

    [Modify] repository 레이어에서의 에러처리 제거
    routes에서 try-catch를 통한 에러처리를 한 뒤 next 호출을 통해
    에러핸들러로 보내버리도록 구현하였기 때문에 굳이 repository에서까지
    에러처리를 할 필요성을 못느껴서 제거하였습니다.

--- server/repository/user.repository.js
+++ server/repository/user.repository.js
@@ -2,11 +2,6 @@ const User = require('../models/user.js');
 
 module.exports = {
   async signUpNewUser(id, pw, name) {
-    try {
-      await User.create({ id, pw, name });
-      return true;
-    } catch (error) {
-      console.error(error);
-    }
+    await User.create({ id, pw, name });
   },
 };
