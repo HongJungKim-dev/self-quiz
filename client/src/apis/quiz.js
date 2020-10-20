0e533780fe189f9e286c25fa6c4e6e458ac3ab19
woomin <dal96k@gmail.com>
2020-10-20 16:58:38 +0900

    [Refactor] 헤더옵션을 분리하여 객체로 저장
    토큰을 넣어주는 헤더 옵션이 계속 중복될 것이라고 판단하여 이를 따로
    분리해주었습니다.

--- client/src/apis/quiz.js
+++ client/src/apis/quiz.js
@@ -2,18 +2,17 @@ import axios from 'axios';
 
 import { URL } from '../config';
 
+const requestHeaders = {
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem('token')}`,
+  },
+};
+
 export default {
-  // TODO: 리팩터링해야함. 헤더 중복될것임
   async addQuiz(question, answer, tags) {
     try {
-      const { data } = await axios.post(`${URL}/quiz`,
-        { question, answer, tags },
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`,
-          },
-        });
-
+      const body = { question, answer, tags };
+      const { data } = await axios.post(`${URL}/quiz`, body, requestHeaders);
       const { newQuiz } = data;
 
       return newQuiz;
