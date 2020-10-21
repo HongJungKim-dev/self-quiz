18306eae43c8e121f061d535fc410be8a98b00e8
woomin <dal96k@gmail.com>
2020-10-21 16:51:57 +0900

    [Feat] 빌드 파일에 해쉬값 추가

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.jsx',
   output: {
     path: path.resolve(__dirname, '../server/public/'),
-    filename: 'main.js',
+    filename: 'main.js[hash]',
   },
   module: {
     rules: [
