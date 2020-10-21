2308447d3b1071135d24cd67492b69eea4f507f6
woomin <dal96k@gmail.com>
2020-10-21 09:14:53 +0900

    [Modify] 아카이브 컴포넌트의 useEffect 로직 이동
    useEffect를 사용해 최초 데이터를 불러오는 것은 컴포넌트보단 페이지에
    있는 것이 옳다고 판단하여 ArchivePage로 로직을 이동시켰습니다.

--- client/src/components/Archive.jsx
+++ client/src/components/Archive.jsx
@@ -1,28 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import { useDispatch, useSelector } from 'react-redux';
-
-import { setQuizzes } from '../slice';
-
-import api from '../apis/quiz';
+import { useSelector } from 'react-redux';
 
 import ArchiveQuiz from './ArchiveQuiz';
 
-const getDataFromServer = async (dispatch) => {
-  const data = await api.getQuizzes();
-
-  dispatch(setQuizzes(data));
-};
-
 export default function Archive() {
-  const dispatch = useDispatch();
   const quizzes = useSelector(({ selfQuizReducer }) => selfQuizReducer.quizzes);
-
-  useEffect(() => {
-    if (quizzes.length === 0) {
-      getDataFromServer(dispatch);
-    }
-  }, []);
 
   return (
     <>
--- client/src/pages/ArchivePage.jsx
+++ client/src/pages/ArchivePage.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
 
 import Archive from '../components/Archive';
 
+import { setQuizzes } from '../slice';
+
+import api from '../apis/quiz';
+
+const getDataFromServer = async (dispatch) => {
+  const data = await api.getQuizzes();
+
+  dispatch(setQuizzes(data));
+};
+
 export default function ArchivePage() {
+  const dispatch = useDispatch();
+  const quizzes = useSelector(({ selfQuizReducer }) => selfQuizReducer.quizzes);
+
+  useEffect(() => {
+    if (quizzes.length === 0) {
+      getDataFromServer(dispatch);
+    }
+  }, []);
+
   return <Archive />;
 }
