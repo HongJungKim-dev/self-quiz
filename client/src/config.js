311390aeba603be859749476f7d5075c8ed119a9
woomin <dal96k@gmail.com>
2020-10-22 17:45:13 +0900

    [Modify] 서버가 HTTPS로 변경됨

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 export const URL = 'http://localhost:3000';
-// export const URL = 'http://self-quiz.herokuapp.com';
+// export const URL = 'https://self-quiz.herokuapp.com';
 
 // TODO: 제거
 const xxx = 'xxx';
