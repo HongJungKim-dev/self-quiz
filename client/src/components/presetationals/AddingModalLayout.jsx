91f885a0387b51f030792f8950230dcd5e0d108e
woomin <dal96k@gmail.com>
2020-11-10 09:26:33 +0900

    [Refactor] 퀴즈 폼의 레이아웃 CSS를 따로 분리
    레이아웃 CSS를 분리하지 않으면 컨테이너가 이를 가지고 있게 될 것 같아서 이를 분리해주었습니다.

---
+++ client/src/components/presetationals/AddingModalLayout.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const styles = {
+  layout: {
+    top: '15%',
+    left: '50%',
+    transform: 'translate(-50%, 0)',
+    position: 'absolute',
+    width: '40rem',
+    height: '40rem',
+    borderRadius: '0.5rem',
+    background: 'white',
+    boxShadow: '0 0 11px 0px #244a72',
+    zIndex: 2,
+  },
+  title: {
+    fontSize: '1.5rem',
+    fontWeight: 'bold',
+    background: '#244a72',
+    color: 'white',
+    padding: '0.4rem 1rem',
+    boxSizing: 'border-box',
+    borderRadius: '0.5rem 0.5rem 0 0',
+  },
+  body: {
+    padding: '2rem',
+  },
+};
+
+export default function AddingModalLayout({ title, children }) {
+  return (
+    <div css={styles.layout}>
+      <div css={styles.title}>{title}</div>
+      <div css={styles.body}>
+        {children}
+      </div>
+    </div>
+  );
+}
