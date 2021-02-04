bf39f268b05a8ab4fb207ee41694efa7f858234d
woomin <dal96k@gmail.com>
2021-02-04 17:44:55 +0900

    [Feat] 돌아가기 버튼을 아이콘으로 대체하고 반응형 처리
    돌아가기 버튼 역시 아이콘으로 대체하였습니다.

--- client/src/components/presentationals/ArchiveBackButton.jsx
+++ client/src/components/presentationals/ArchiveBackButton.jsx
@@ -1,32 +1,33 @@
 import React from 'react';
 
-import { color, size } from '../../static';
+import { color } from '../../static';
+
+import ReturningIcon from '../icons/Returningcon';
 
 const styles = {
   button: {
     position: 'fixed',
-    width: '7rem',
-    height: '3rem',
-    fontSize: size.smallText,
-    fontWeight: 'bold',
+    width: '2rem',
     color: color.font,
-    background: `linear-gradient(90deg, #000000, ${color.dark})`,
-    border: 'none',
-    borderRadius: '0.6rem',
     cursor: 'pointer',
-    margin: '2rem 1rem',
-    boxShadow: `0 0 8px 0px ${color.faintWhite}`,
+    margin: '2rem',
+    '&:hover': {
+      color: color.faintWhite,
+    },
+    '@media (max-width: 768px)': {
+      width: '5rem',
+    },
   },
 };
 
 export default function ArchiveBackButton({ onClick }) {
   return (
-    <button
-      type="button"
-      onClick={onClick}
+    <div
+      title="돌아가기"
       css={styles.button}
+      onClick={onClick}
     >
-      Back
-    </button>
+      <ReturningIcon />
+    </div>
   );
 }
