62d28c8e6bd95b68611301e23aae86e0b53dd15f
woomin <dal96k@gmail.com>
2021-02-27 21:41:14 +0900

    [Feat] 회원가입 버튼에 대한 로직 구현
    회원가입 버튼을 클릭하면 여러 조건을 확인한 뒤 회원가입이 성공적으로 이루어질 수 있도록 구현하였습니다.

--- client/src/components/containers/SignupFormContainer.jsx
+++ client/src/components/containers/SignupFormContainer.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { setSignupForm } from '../../slice';
+
+import { useHistory } from 'react-router-dom';
+
+import { resetSignupForm, setSignupForm } from '../../slice';
+
+import { popupMessages } from '../../util';
+
+import api from '../../apis/api';
 
 import LoginFormLayout from '../layouts/LoginFormLayout';
 import LoginFormLogo from '../presentationals/LoginFormLogo';
@@ -29,7 +36,47 @@ export default function SignupFormContainer() {
     dispatch(setSignupForm({ type: 'pw2', value: event.target.value }));
   };
 
-  const handleSignupButtonClick = () => {};
+  const history = useHistory();
+
+  const handleSignupButtonClick = async () => {
+    if (!name || !id || !pw1 || !pw2) {
+      await popupMessages.fail('모든 항목을 입력하셔야 합니다.');
+      return;
+    }
+
+    if (id.length < 4) {
+      await popupMessages.fail('아이디는 4글자 이상으로 설정하셔야 합니다.');
+      return;
+    }
+
+    if (pw1.length < 4) {
+      await popupMessages.fail('비밀번호는 4글자 이상으로 설정하셔야 합니다.');
+      return;
+    }
+
+    if (pw1 !== pw2) {
+      await popupMessages.fail('두 비밀번호가 서로 다릅니다.');
+      return;
+    }
+
+    const isValid = await api.checkId(id);
+
+    if (!isValid) {
+      await popupMessages.fail('이미 사용중인 아이디입니다.');
+      return;
+    }
+
+    const state = await api.signUp(id, pw1, name);
+
+    if (!state) {
+      await popupMessages.fail('뭔가 단단히 잘못됐음');
+      return;
+    }
+
+    await popupMessages.success('회원가입 성공! 다시 로그인해주세요.');
+    dispatch(resetSignupForm());
+    history.push('/');
+  };
 
   return (
     <LoginFormLayout>
