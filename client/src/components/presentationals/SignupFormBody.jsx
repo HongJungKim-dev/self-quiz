5579b6135b5e6f7f710777029dfcda47d5e1fda8
woomin <dal96k@gmail.com>
2021-02-27 20:54:09 +0900

    [Feat] SignupFormContainer에 있던 UI 로직을 분리
    SignupFormBody라는 presentational 컴포넌트를 만들어서 분리하였습니다.

---
+++ client/src/components/presentationals/SignupFormBody.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { color, mobileSize, size } from '../../static';
+
+const styles = {
+  input: {
+    display: 'block',
+    fontSize: size['#5'],
+    width: '96%',
+    height: '3rem',
+    border: 'none',
+    borderBottom: `1px solid ${color.light}`,
+    paddingLeft: '1rem',
+    marginTop: '1px',
+    '@media (max-width: 768px)': {
+      fontSize: mobileSize['#6'],
+      height: '6rem',
+    },
+    '@media (max-width: 480px)': {
+      fontSize: mobileSize['#4'],
+      height: '10rem',
+    },
+  },
+  button: {
+    fontSize: size['#5'],
+    fontWeight: 'bold',
+    color: color.font,
+    width: '100%',
+    height: '3rem',
+    border: 'none',
+    borderRadius: '0.5rem',
+    marginTop: '0.5rem',
+    cursor: 'pointer',
+    backgroundColor: color.main,
+    '@media (max-width: 768px)': {
+      fontSize: mobileSize['#6'],
+      height: '6rem',
+    },
+    '@media (max-width: 480px)': {
+      fontSize: mobileSize['#4'],
+      height: '10rem',
+      marginTop: '2rem',
+    },
+  },
+};
+
+export default function SignupFormBody() {
+  return (
+    <div>
+      <input
+        value=""
+        css={styles.input}
+        onChange
+        placeholder="이름을 입력해주세요."
+      />
+      <input
+        value=""
+        css={styles.input}
+        onChange
+        placeholder="아이디를 입력해주세요."
+      />
+      <input
+        value=""
+        css={styles.input}
+        onChange
+        type="password"
+        placeholder="비밀번호를 입력해주세요."
+      />
+      <input
+        value=""
+        css={styles.input}
+        onChange
+        type="password"
+        placeholder="비밀번호를 다시 입력해주세요."
+      />
+      <button
+        type="button"
+        css={styles.button}
+        onClick
+      >회원가입
+      </button>
+    </div>
+  );
+}
